033fffec26be2402e27c7c09c378e3c042d95544
Vladislav Sumin <vladislavsumin@mail.ru>
2025-08-28 00:34:55 +0300

    setup compose && decompose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("ru.vladislavsumin.convention.kmp.jvm")
+    id("ru.vladislavsumin.convention.compose")
 }
 
 kotlin {
@@ -7,6 +8,25 @@ kotlin {
         commonMain.dependencies {
             implementation(vsCoreLibs.vs.core.logger.api)
             implementation(vsCoreLibs.vs.core.logger.platform)
+            implementation(vsCoreLibs.vs.core.decompose.components)
+            implementation(vsCoreLibs.vs.core.decompose.compose)
+
+            implementation(vsCoreLibs.decompose.extensions.compose)
+            implementation(vsCoreLibs.decompose.extensions.composeExperimental)
+
+            implementation(compose.material3)
+        }
+
+        jvmMain.dependencies {
+            // Тянет набор библиотек для текущей платформы.
+            // Тянет за собой кучу других compose библиотек, в том числе material.
+            // Эта библиотека подключает специфичные апи заточенные под конкретную OS и конкретную архитектуру,
+            // поэтому данный код не запустится на других платформах, кроме той на которой его собрали.
+            // TODO сделать разделение по платформам.
+            implementation(compose.desktop.currentOs)
+
+            // Реализует Dispatchers.Main для Swing.
+            implementation(vsCoreLibs.kotlin.coroutines.swing)
         }
     }
 }
--- app/src/commonMain/kotlin/ru/vladislavsumin/qa/Main.kt
+++ app/src/commonMain/kotlin/ru/vladislavsumin/qa/Main.kt
@@ -1,13 +1,70 @@
 package ru.vladislavsumin.qa
 
+import androidx.compose.material3.Text
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import com.arkivanov.decompose.DefaultComponentContext
+import com.arkivanov.decompose.extensions.compose.lifecycle.LifecycleController
+import com.arkivanov.essenty.lifecycle.LifecycleRegistry
 import ru.vladislavsumin.core.logger.manager.LoggerManager
 import ru.vladislavsumin.core.logger.platform.initDefault
+import javax.swing.SwingUtilities
 import kotlin.system.exitProcess
 
 fun main() {
     setExitOnUncaughtException()
+
     LoggerManager.initDefault()
     MainLogger.i("Initialization...")
+
+    // Создаем рутовый Decompose lifecycle.
+    val lifecycle = LifecycleRegistry()
+
+    val context = runOnUiThread {
+        DefaultComponentContext(lifecycle)
+    }
+
+    application {
+        // Связываем рутовый Decompose lifecycle с жизненным циклом окна.
+        val windowState = rememberWindowState()
+        LifecycleController(lifecycle, windowState)
+
+        Window(
+            title = "vs-qa",
+            onCloseRequest = ::exitApplication,
+        ) {
+            Text("Hello world")
+        }
+    }
+
+}
+
+/**
+ * See https://arkivanov.github.io/Decompose/getting-started/quick-start/
+ * See https://github.com/arkivanov/Decompose/blob/master/sample/app-desktop/src/jvmMain/kotlin/com/arkivanov/sample/app/Utils.kt
+ */
+@Suppress("TooGenericExceptionCaught")
+internal fun <T> runOnUiThread(block: () -> T): T {
+    if (SwingUtilities.isEventDispatchThread()) {
+        return block()
+    }
+
+    var error: Throwable? = null
+    var result: T? = null
+
+    SwingUtilities.invokeAndWait {
+        try {
+            result = block()
+        } catch (e: Throwable) {
+            error = e
+        }
+    }
+
+    error?.also { throw it }
+
+    @Suppress("UNCHECKED_CAST")
+    return result as T
 }
 
 fun setExitOnUncaughtException() {
