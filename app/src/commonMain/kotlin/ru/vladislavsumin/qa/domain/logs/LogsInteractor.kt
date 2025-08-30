3579f12555b219d95cedc45e6d4edbc30ba1ca97
Vladislav Sumin <vladislavsumin@mail.ru>
2025-08-30 15:22:31 +0300

    add log filter

--- app/src/commonMain/kotlin/ru/vladislavsumin/qa/domain/logs/LogsInteractor.kt
+++ app/src/commonMain/kotlin/ru/vladislavsumin/qa/domain/logs/LogsInteractor.kt
@@ -1,12 +1,16 @@
 package ru.vladislavsumin.qa.domain.logs
 
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.joinAll
 import ru.vladislavsumin.qa.LogLogger
 import ru.vladislavsumin.qa.utils.measureTimeMillisWithResult
 import java.nio.file.Path
 
 
 interface LogsInteractor {
-    fun filterLogs(filter: String): List<RawLogRecord>
+    suspend fun filterLogs(filter: String): List<RawLogRecord>
 }
 
 class LogsInteractorImpl(
@@ -18,7 +22,9 @@ class LogsInteractorImpl(
         return AnimeLogParser().parseLog(logPath)
     }
 
-    override fun filterLogs(filter: String): List<RawLogRecord> {
+    override suspend fun filterLogs(filter: String): List<RawLogRecord> {
+        // На таком простом фильтре наивный многопоточный фильтр дает выигрыш порядка 40%
+        // Производительность фильтра порядка 8кк записаей в секунду
         val (time, result) = measureTimeMillisWithResult { logs.filter { it.raw.contains(filter) } }
         LogLogger.d { "Log filtered at ${time}ms, size = ${result.size}" }
         return result
