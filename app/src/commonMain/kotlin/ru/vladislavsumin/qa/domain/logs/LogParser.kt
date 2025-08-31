f1e6c774cbbc17d747edbb4b0e1c3970e36f3af9
Vladislav Sumin <vladislavsumin@mail.ru>
2025-09-01 00:40:15 +0300

    add zip support

--- app/src/commonMain/kotlin/ru/vladislavsumin/qa/domain/logs/LogParser.kt
+++ app/src/commonMain/kotlin/ru/vladislavsumin/qa/domain/logs/LogParser.kt
@@ -2,7 +2,11 @@ package ru.vladislavsumin.qa.domain.logs
 
 import ru.vladislavsumin.qa.LogParserLogger
 import java.nio.file.Path
+import java.util.zip.ZipFile
+import java.util.zip.ZipInputStream
+import kotlin.io.path.absolutePathString
 import kotlin.io.path.bufferedReader
+import kotlin.io.path.extension
 import kotlin.system.measureTimeMillis
 
 interface LogParser {
@@ -16,38 +20,65 @@ class AnimeLogParser() : LogParser {
 
         val result = mutableListOf<RawLogRecord>()
         val totalParseTime = measureTimeMillis {
-            val lines = filePath.bufferedReader().lineSequence()
-            var cache: RawLogRecord? = null
-            var order = 0
-            for ((index, line) in lines.withIndex()) {
-                val matches = LOG_REGEX.matchEntire(line)
-                if (matches != null) {
-                    cache?.let(result::add)
-                    cache = RawLogRecord(
-                        line = index,
-                        order = ++order,
-                        raw = line,
-                        time = matches.groups[1]!!.range,
-                        thread = matches.groups[2]!!.range,
-                        level = matches.groups[3]!!.range,
-                        tag = matches.groups[4]!!.range,
-                        message = matches.groups[5]!!.range,
-                    )
-                } else {
-                    val oldCache = cache!!
-                    cache = oldCache.copy(
-                        raw = oldCache.raw + "\n" + line,
-                        message = IntRange(
-                            start = oldCache.message.start,
-                            endInclusive = oldCache.message.last + line.length + 1,
-                        )
-                    )
-                }
+            if (filePath.extension == "zip") {
+                parseZip(filePath, result)
+            } else {
+                parseLogFile(filePath, result)
             }
-            result.add(cache!!)
         }
         LogParserLogger.d { "Parsed file $filePath at ${totalParseTime}ms. Lines = ${result.last().line}, logs = ${result.size}}" }
         return result
+    }
+
+    private fun parseZip(filePath: Path, result: MutableList<RawLogRecord>) {
+        val zip = ZipFile(filePath.absolutePathString())
+        val names = zip.entries().toList().map { it.name }.sorted()
+        names.forEach {
+            zip.getInputStream(zip.getEntry(it)).use { internalZipStream ->
+                ZipInputStream(internalZipStream).use { zipStream ->
+                    zipStream.nextEntry!!
+                    val lines = zipStream.bufferedReader().lineSequence()
+                    parseLines(lines, result)
+                }
+            }
+        }
+    }
+
+    private fun parseLogFile(filePath: Path, result: MutableList<RawLogRecord>) {
+        val lines = filePath.bufferedReader().lineSequence()
+        parseLines(lines, result)
+    }
+
+    private fun parseLines(lines: Sequence<String>, result: MutableList<RawLogRecord>) {
+        var cache: RawLogRecord? = null
+        var order = 0
+        for ((index, line) in lines.withIndex()) {
+            val matches = LOG_REGEX.matchEntire(line)
+            if (matches != null) {
+                cache?.let(result::add)
+                cache = RawLogRecord(
+                    line = index,
+                    order = ++order,
+                    raw = line,
+                    time = matches.groups[1]!!.range,
+                    thread = matches.groups[2]!!.range,
+                    level = matches.groups[3]!!.range,
+                    tag = matches.groups[4]!!.range,
+                    message = matches.groups[5]!!.range,
+                )
+            } else {
+                println(line)
+                val oldCache = cache!!
+                cache = oldCache.copy(
+                    raw = oldCache.raw + "\n" + line,
+                    message = IntRange(
+                        start = oldCache.message.start,
+                        endInclusive = oldCache.message.last + line.length + 1,
+                    )
+                )
+            }
+        }
+        result.add(cache!!)
     }
 
     companion object {
