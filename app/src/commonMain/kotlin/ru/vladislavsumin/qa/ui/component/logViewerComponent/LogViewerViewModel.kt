cf181a6f7a78090bf24efd40bd9133fb9b3a96c4
Vladislav Sumin <vladislavsumin@mail.ru>
2025-09-01 22:35:14 +0300

    cleanup LogViewerViewModel.kt

--- app/src/commonMain/kotlin/ru/vladislavsumin/qa/ui/component/logViewerComponent/LogViewerViewModel.kt
+++ app/src/commonMain/kotlin/ru/vladislavsumin/qa/ui/component/logViewerComponent/LogViewerViewModel.kt
@@ -3,10 +3,11 @@ package ru.vladislavsumin.qa.ui.component.logViewerComponent
 import androidx.compose.runtime.Stable
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.onEach
 import ru.vladislavsumin.core.decompose.components.ViewModel
-import ru.vladislavsumin.qa.domain.logs.LogRecord
 import ru.vladislavsumin.qa.domain.logs.LogsInteractorImpl
 import java.nio.file.Path
 
@@ -22,62 +23,32 @@ internal class LogViewerViewModel(
     private val visibleIndexes = MutableStateFlow(Pair(0, 0))
 
     private val logsInteractor = LogsInteractorImpl(logPath)
-    private val internalState = combine(
-        logsInteractor.observeLogIndex(filter, search),
+
+    val state = combine(
+        logsInteractor.observeLogIndex(filter, search)
+            .onEach {
+                // TODO убрать эту жесть.
+                selectedSearchIndex.value = 0
+                if (it.lastSuccessIndex.searchIndex.index.isNotEmpty()) {
+                    scrollToIndex(it.lastSuccessIndex.searchIndex.index[selectedSearchIndex.value])
+                }
+            },
         filter,
         search,
         isFilterUseRegex,
         isSearchUseRegex,
-    ) { logIndexProgress, filter, search, isFilterUseRegex, isSearchUseRegex ->
-        // TODO весь код ниже это пока один большой костыль. Работает на честном слове.
-
-        val logsIndex = logIndexProgress.lastSuccessIndex
-        val filteredLogs = logsIndex.logs
-        val searchIndex = logsIndex.searchIndex.index
-
-        selectedSearchIndex.value = 0
-
-        if (searchIndex.isNotEmpty()) {
-            scrollToIndex(searchIndex[selectedSearchIndex.value])
-        }
-
-        IntermediateState(
+        selectedSearchIndex,
+    ) { logIndexProgress, filter, search, isFilterUseRegex, isSearchUseRegex, selectedSearchIndex ->
+        LogViewerViewState(
             filter = filter,
             search = search,
             isFilterUseRegex = isFilterUseRegex,
             isSearchUseRegex = isSearchUseRegex,
-            searchResults = searchIndex.size,
-            searchIndex = searchIndex,
-            logs = filteredLogs,
-            maxLogNumberDigits = logsInteractor.logs.last().order.toString().length,
-        )
-    }.stateIn(
-        IntermediateState(
-            filter = "",
-            search = "",
-            isFilterUseRegex = false,
-            isSearchUseRegex = false,
-            searchResults = 0,
-            searchIndex = emptyList(),
-            logs = emptyList(),
-            maxLogNumberDigits = 0,
-        ),
-    )
-
-    val state = combine(
-        internalState,
-        selectedSearchIndex,
-    ) { internalState, selectedSearchIndex ->
-        LogViewerViewState(
-            filter = internalState.filter,
-            search = internalState.search,
-            isFilterUseRegex = internalState.isFilterUseRegex,
-            isSearchUseRegex = internalState.isSearchUseRegex,
-            searchResults = internalState.searchResults,
+            searchResults = logIndexProgress.lastSuccessIndex.searchIndex.index.size,
             selectedSearchIndex = selectedSearchIndex,
-            searchIndex = internalState.searchIndex,
-            logs = internalState.logs,
-            maxLogNumberDigits = internalState.maxLogNumberDigits,
+            searchIndex = logIndexProgress.lastSuccessIndex.searchIndex.index,
+            logs = logIndexProgress.lastSuccessIndex.logs,
+            maxLogNumberDigits = logsInteractor.logs.last().order.toString().length,
         )
     }
         .stateIn(
@@ -104,17 +75,17 @@ internal class LogViewerViewModel(
     }
 
     fun onClickPrevIndex() {
-        if (internalState.value.searchIndex.isNotEmpty()) {
+        if (state.value.searchIndex.isNotEmpty()) {
             selectedSearchIndex.value = (selectedSearchIndex.value - 1).coerceAtLeast(0)
-            scrollToIndex(internalState.value.searchIndex[selectedSearchIndex.value])
+            scrollToIndex(state.value.searchIndex[selectedSearchIndex.value])
         }
     }
 
     fun onClickNextIndex() {
-        if (internalState.value.searchIndex.isNotEmpty()) {
+        if (state.value.searchIndex.isNotEmpty()) {
             selectedSearchIndex.value =
-                (selectedSearchIndex.value + 1).coerceAtMost(internalState.value.searchIndex.size - 1)
-            scrollToIndex(internalState.value.searchIndex[selectedSearchIndex.value])
+                (selectedSearchIndex.value + 1).coerceAtMost(state.value.searchIndex.size - 1)
+            scrollToIndex(state.value.searchIndex[selectedSearchIndex.value])
         }
     }
 
@@ -137,15 +108,25 @@ internal class LogViewerViewModel(
     fun onClickSearchUseRegex(newValue: Boolean) {
         isFilterUseRegex.value = newValue
     }
+}
 
-    private data class IntermediateState(
-        val filter: String,
-        val search: String,
-        val isFilterUseRegex: Boolean,
-        val isSearchUseRegex: Boolean,
-        val searchResults: Int,
-        val searchIndex: List<Int>,
-        val logs: List<LogRecord>,
-        val maxLogNumberDigits: Int,
+// TODO и эту жесть убрать
+@Suppress("MagicNumber")
+private fun <T1, T2, T3, T4, T5, T6, R> combine(
+    flow: Flow<T1>,
+    flow2: Flow<T2>,
+    flow3: Flow<T3>,
+    flow4: Flow<T4>,
+    flow5: Flow<T5>,
+    flow6: Flow<T6>,
+    transform: suspend (T1, T2, T3, T4, T5, T6) -> R,
+): Flow<R> = combine(flow, flow2, flow3, flow4, flow5, flow6) { args: Array<*> ->
+    transform(
+        args[0] as T1,
+        args[1] as T2,
+        args[2] as T3,
+        args[3] as T4,
+        args[4] as T5,
+        args[5] as T6,
     )
 }
