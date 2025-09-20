4a3b4c047ad2c50998b7bd00dca7323ffa155a55
Vladislav Sumin <vladislavsumin@mail.ru>
2025-09-20 13:23:48 +0300

    Add ksp support

--- build-logic/src/main/kotlin/ru/vladislavsumin/convention/preset/feature-impl.gradle.kts
+++ build-logic/src/main/kotlin/ru/vladislavsumin/convention/preset/feature-impl.gradle.kts
@@ -5,6 +5,7 @@ import ru.vladislavsumin.utils.vsCoreLibs
 plugins {
     id("ru.vladislavsumin.convention.preset.feature-api")
     id("ru.vladislavsumin.convention.impl-to-api-dependency")
+    id("ru.vladislavsumin.convention.kmp.ksp-hack")
 }
 
 kotlin {
@@ -12,6 +13,11 @@ kotlin {
         commonMain.dependencies {
             implementation(project(":core:utils"))
             implementation(vsCoreLibs.vs.core.di)
+            implementation(vsCoreLibs.vs.core.factoryGenerator.api)
         }
     }
+}
+
+dependencies {
+    add("kspCommonMainMetadata", vsCoreLibs.vs.core.factoryGenerator.ksp)
 }
