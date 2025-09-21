5c97e16584520c569f5eff7b484830f3738770b3
Vladislav Sumin <vladislavsumin@mail.ru>
2025-09-21 20:41:17 +0300

    Add styles for tabs

--- feature/root-screen/impl/src/commonMain/kotlin/ru/vladislavsumin/qa/feature/rootScreen/ui/screen/root/RootScreen.kt
+++ feature/root-screen/impl/src/commonMain/kotlin/ru/vladislavsumin/qa/feature/rootScreen/ui/screen/root/RootScreen.kt
@@ -1,8 +1,13 @@
 package ru.vladislavsumin.qa.feature.rootScreen.ui.screen.root
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Icon
@@ -10,7 +15,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.movableContentOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.arkivanov.decompose.ComponentContext
 import com.arkivanov.decompose.childContext
 import com.arkivanov.decompose.extensions.compose.pages.ChildPages
@@ -20,6 +27,7 @@ import ru.vladislavsumin.core.navigation.factoryGenerator.GenerateScreenFactory
 import ru.vladislavsumin.core.navigation.host.childNavigationPages
 import ru.vladislavsumin.core.navigation.screen.Screen
 import ru.vladislavsumin.core.ui.button.QaIconButton
+import ru.vladislavsumin.core.ui.designSystem.theme.QaTheme
 import ru.vladislavsumin.feature.logViewer.ui.screen.logViewer.LogViewerScreenFactory
 import ru.vladislavsumin.feature.logViewer.ui.screen.logViewer.LogViewerScreenParams
 import ru.vladislavsumin.qa.feature.bottomBar.ui.component.bottomBar.BottomBarComponentFactory
@@ -52,16 +60,33 @@ internal class RootScreen(
     @Composable
     override fun Render(modifier: Modifier) {
         Column(modifier) {
-            Row {
-                val pages = tabs.subscribeAsState().value
-                if (pages.items.size > 1) {
-                    pages.items.forEach {
-                        Text(
-                            text = (it.configuration.screenParams as LogViewerScreenParams).logPath.name,
-                            Modifier.clickable(onClick = { navigator.open(it.configuration.screenParams) }),
-                        )
-                        QaIconButton(onClick = { navigator.close(it.configuration.screenParams) }) {
-                            Icon(imageVector = Icons.Default.Close, contentDescription = "close")
+            val pages = tabs.subscribeAsState().value
+            if (pages.items.size > 1) {
+                LazyRow {
+                    itemsIndexed(pages.items, key = { _, item -> item.configuration.screenParams }) { index, item ->
+                        Row(
+                            modifier = Modifier
+                                .background(
+                                    if (index == pages.selectedIndex) {
+                                        QaTheme.colorScheme.surfaceVariant
+                                    } else {
+                                        QaTheme.colorScheme.surface
+                                    },
+                                )
+                                .clickable(onClick = { navigator.open(item.configuration.screenParams) }),
+                            verticalAlignment = Alignment.CenterVertically,
+
+                        ) {
+                            Text(
+                                text = (item.configuration.screenParams as LogViewerScreenParams).logPath.name,
+                                modifier = Modifier.padding(start = 8.dp, end = 4.dp),
+                            )
+                            QaIconButton(
+                                onClick = { navigator.close(item.configuration.screenParams) },
+                                modifier = Modifier.padding(end = 4.dp),
+                            ) {
+                                Icon(imageVector = Icons.Default.Close, contentDescription = "close")
+                            }
                         }
                     }
                 }
