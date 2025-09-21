fa5418d61bc50155f5d01aa4621d1694db0c143b
Vladislav Sumin <vladislavsumin@mail.ru>
2025-09-21 18:22:59 +0300

    add tabs

--- feature/root-screen/impl/src/commonMain/kotlin/ru/vladislavsumin/qa/feature/rootScreen/ui/screen/root/RootScreen.kt
+++ feature/root-screen/impl/src/commonMain/kotlin/ru/vladislavsumin/qa/feature/rootScreen/ui/screen/root/RootScreen.kt
@@ -1,18 +1,29 @@
 package ru.vladislavsumin.qa.feature.rootScreen.ui.screen.root
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.movableContentOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.arkivanov.decompose.ComponentContext
 import com.arkivanov.decompose.childContext
+import com.arkivanov.decompose.extensions.compose.pages.ChildPages
 import com.arkivanov.decompose.extensions.compose.subscribeAsState
+import com.arkivanov.decompose.router.pages.Pages
 import ru.vladislavsumin.core.navigation.factoryGenerator.GenerateScreenFactory
-import ru.vladislavsumin.core.navigation.host.childNavigationSlot
+import ru.vladislavsumin.core.navigation.host.childNavigationPages
 import ru.vladislavsumin.core.navigation.screen.Screen
+import ru.vladislavsumin.core.ui.button.QaIconButton
 import ru.vladislavsumin.feature.logViewer.ui.screen.logViewer.LogViewerScreenFactory
+import ru.vladislavsumin.feature.logViewer.ui.screen.logViewer.LogViewerScreenParams
 import ru.vladislavsumin.qa.feature.bottomBar.ui.component.bottomBar.BottomBarComponentFactory
+import kotlin.io.path.name
 
 @GenerateScreenFactory
 internal class RootScreen(
@@ -33,19 +44,41 @@ internal class RootScreen(
         }
     }
 
-    private val tabs = childNavigationSlot(TabNavigationHost)
+    private val tabs = childNavigationPages(
+        navigationHost = TabNavigationHost,
+        initialPages = { Pages() },
+    )
 
     @Composable
     override fun Render(modifier: Modifier) {
         Column(modifier) {
-            val child = tabs.subscribeAsState().value.child
-            // TODO разобраться почему это работает?
-            val hackyContent = remember(child) {
-                movableContentOf {
-                    child?.instance?.Render(Modifier.weight(1f))
+            Row {
+                val pages = tabs.subscribeAsState().value
+                if (pages.items.size > 1) {
+                    pages.items.forEach {
+                        Text(
+                            text = (it.configuration.screenParams as LogViewerScreenParams).logPath.name,
+                            Modifier.clickable(onClick = { navigator.open(it.configuration.screenParams) }),
+                        )
+                        QaIconButton(onClick = { navigator.close(it.configuration.screenParams) }) {
+                            Icon(imageVector = Icons.Default.Close, contentDescription = "close")
+                        }
+                    }
                 }
             }
-            hackyContent()
+            ChildPages(
+                tabs,
+                onPageSelected = { _ -> },
+                Modifier.weight(1f),
+            ) { _, page ->
+                // TODO разобраться почему это работает?
+                val hackyContent = remember(page) {
+                    movableContentOf {
+                        page.Render(Modifier)
+                    }
+                }
+                hackyContent()
+            }
             bottomBarComponent.Render(Modifier)
         }
     }
