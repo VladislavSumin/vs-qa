2fc1913454cfc850c89c3eda20ae1a9bb3f7380c
Vladislav Sumin <vladislavsumin@mail.ru>
2025-11-27 19:08:54 +0300

    Split file picker dialog by different os

--- core/ui/file-picker/src/commonMain/kotlin/ru/vladislavsumin/core/ui/filePicker/FilePickerDialog.kt
+++ core/ui/file-picker/src/commonMain/kotlin/ru/vladislavsumin/core/ui/filePicker/FilePickerDialog.kt
@@ -1,29 +1,7 @@
 package ru.vladislavsumin.core.ui.filePicker
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.window.AwtWindow
-import java.awt.FileDialog
-import java.awt.Frame
 import java.nio.file.Path
-import kotlin.io.path.Path
 
-@OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun FilePickerDialog(onCloseRequest: (result: Path?) -> Unit) = AwtWindow(
-    create = {
-        object : FileDialog(null as Frame?, "Choose a file", LOAD) {
-            override fun setVisible(value: Boolean) {
-                super.setVisible(value)
-                if (value) {
-                    if (directory != null && file != null) {
-                        onCloseRequest(Path(directory + file))
-                    } else {
-                        onCloseRequest(null)
-                    }
-                }
-            }
-        }
-    },
-    dispose = FileDialog::dispose,
-)
+expect fun FilePickerDialog(onCloseRequest: (result: Path?) -> Unit)
