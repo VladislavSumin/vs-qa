f4dbd94d69f88d020e358a9d536d67c148122bde
Vladislav Sumin <vladislavsumin@mail.ru>
2025-12-08 19:46:08 +0300

    Fix temp fix for crash when save instance on android

--- feature/log-viewer/api/src/commonMain/kotlin/ru/vladislavsumin/feature/logViewer/ui/screen/logViewer/LogViewerScreenParams.kt
+++ feature/log-viewer/api/src/commonMain/kotlin/ru/vladislavsumin/feature/logViewer/ui/screen/logViewer/LogViewerScreenParams.kt
@@ -1,12 +1,19 @@
 package ru.vladislavsumin.feature.logViewer.ui.screen.logViewer
 
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import ru.vladislavsumin.core.navigation.IntentScreenParams
 import ru.vladislavsumin.core.navigation.ScreenIntent
 import java.nio.file.Path
+import kotlin.io.path.Path
 
 @Serializable
 data class LogViewerScreenParams(
+    @Serializable(PathSerializer::class)
     val logPath: Path,
 ) : IntentScreenParams<LogViewerScreenIntent>
 
@@ -15,4 +22,18 @@ sealed interface LogViewerScreenIntent : ScreenIntent {
      * Применяет или заменяет текущий mapping.
      */
     data class OpenMapping(val mappingPath: Path) : LogViewerScreenIntent
+}
+
+// TODO временный фикс для андроид что бы не падать при сериализации, полноценный фикс нужен в навигации.
+private object PathSerializer : KSerializer<Path> {
+    override val descriptor: SerialDescriptor
+        get() = String.serializer().descriptor
+
+    override fun serialize(encoder: Encoder, value: Path) {
+        encoder.encodeString(value.toString())
+    }
+
+    override fun deserialize(decoder: Decoder): Path {
+        return Path(decoder.decodeString())
+    }
 }
