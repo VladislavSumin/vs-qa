7b5620a16e307ea8ef2ef3b4155de46b32d503db
Vladislav Sumin <vladislavsumin@mail.ru>
2026-05-30 16:22:10 +0300

    Fix properties mirroring for included builds

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -1,3 +1,16 @@
+val rootProps = java.util.Properties()
+file("../gradle.properties").inputStream().use { rootProps.load(it) }
+rootProps.forEach { (k, v) ->
+    val key = k.toString()
+    if (!extra.has(key)) extra.set(key, v.toString())
+}
+
+gradle.beforeProject {
+    settings.extra.properties.forEach { (k, v) ->
+        if (!project.hasProperty(k as String)) project.extensions.extraProperties.set(k, v)
+    }
+}
+
 val useVsCoreSources = extra["ru.vs.core.useVsCoreSources"].toString().toBoolean()
 if (useVsCoreSources) {
     includeBuild("../../vs-core/build-scripts")
