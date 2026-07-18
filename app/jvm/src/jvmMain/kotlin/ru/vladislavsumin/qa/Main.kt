2145b169b7653e40f28bbdc927e6e44bb5c1bd09
Vladislav Sumin <vladislavsumin@mail.ru>
2026-07-18 22:20:11 +0300

    Add state saving debug for jvm mvp

--- app/jvm/src/jvmMain/kotlin/ru/vladislavsumin/qa/Main.kt
+++ app/jvm/src/jvmMain/kotlin/ru/vladislavsumin/qa/Main.kt
@@ -1,19 +1,35 @@
 package ru.vladislavsumin.qa
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.LocalSaveableStateRegistry
+import androidx.compose.runtime.saveable.SaveableStateRegistry
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.application
+import com.arkivanov.decompose.ComponentContext
 import com.arkivanov.decompose.DefaultComponentContext
 import com.arkivanov.essenty.lifecycle.LifecycleRegistry
 import com.arkivanov.essenty.lifecycle.resume
 import io.sentry.kotlin.multiplatform.Sentry
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 import org.kodein.di.instance
+import ru.vladislavsumin.core.decompose.compose.ComposeComponent
 import ru.vladislavsumin.core.decompose.compose.runOnUiThread
 import ru.vladislavsumin.core.logger.manager.LoggerManager
 import ru.vladislavsumin.feature.mcp.domain.McpServer
 import ru.vladislavsumin.qa.feature.multiWindow.ui.component.multiWindowRootScreen.MultiWindowRootScreenComponentFactory
 import kotlin.io.path.Path
 import kotlin.system.exitProcess
+import kotlin.time.Duration.Companion.seconds
+
+private const val STATE_DEBUG_ENABLED = false
 
 fun main(args: Array<String>) {
     setExitOnUncaughtException()
@@ -35,17 +51,30 @@ fun main(args: Array<String>) {
     val logPath = if (args.isNotEmpty()) Path(args[0]) else null
     val mappingPath = if (args.size > 1) Path(args[1]) else null
 
-    val lifecycle = LifecycleRegistry()
+    if (STATE_DEBUG_ENABLED) {
+        val debugStateManager = DebugStateManager {
+            di.instance<MultiWindowRootScreenComponentFactory>().create(logPath, mappingPath, it)
+        }
+        application {
+            debugStateManager.Intercept { component, lifecycle ->
+                component.Render(Modifier)
+                LaunchedEffect(lifecycle) {
+                    lifecycle.resume()
+                }
+            }
+        }
+    } else {
+        val lifecycle = LifecycleRegistry()
 
-    val rootScreenComponent = runOnUiThread {
-        val context = DefaultComponentContext(lifecycle)
-        di.instance<MultiWindowRootScreenComponentFactory>().create(logPath, mappingPath, context)
-    }
-
-    application {
-        rootScreenComponent.Render(Modifier)
-        LaunchedEffect(lifecycle) {
-            lifecycle.resume()
+        val rootScreenComponent = runOnUiThread {
+            val context = DefaultComponentContext(lifecycle)
+            di.instance<MultiWindowRootScreenComponentFactory>().create(logPath, mappingPath, context)
+        }
+        application {
+            rootScreenComponent.Render(Modifier)
+            LaunchedEffect(lifecycle) {
+                lifecycle.resume()
+            }
         }
     }
 }
@@ -61,5 +90,55 @@ fun setExitOnUncaughtException() {
             throwable.printStackTrace()
         }
         exitProcess(1)
+    }
+}
+
+class DebugStateManager(rootComponentFactory: (ComponentContext) -> ComposeComponent) {
+
+    init {
+        GlobalScope.launch(Dispatchers.Main) {
+            while (true) {
+                delay(10.seconds)
+                save()
+                destroy()
+                delay(2.seconds)
+                restore()
+            }
+        }
+    }
+
+    private val composeSaveableStateRegistryFlow: MutableStateFlow<SaveableStateRegistry?> =
+        MutableStateFlow(createComposeSaveableStateRegistry())
+    private var composeSavedState: Map<String, List<Any?>>? = null
+
+    private var composeContext: ComponentContext? = null
+    private var composeComponent: ComposeComponent? = null
+
+    fun createComposeSaveableStateRegistry(): SaveableStateRegistry = SaveableStateRegistry(
+        restoredValues = composeSavedState,
+        canBeSaved = { true },
+    )
+
+    fun save() {
+        composeSavedState = composeSaveableStateRegistryFlow.value?.performSave()
+    }
+
+    fun destroy() {
+        composeSaveableStateRegistryFlow.value = null
+    }
+
+    fun restore() {
+        composeSaveableStateRegistryFlow.value = createComposeSaveableStateRegistry()
+        composeSavedState = null
+    }
+
+    @Composable
+    fun Intercept(content: @Composable (component: ComposeComponent, decomposeLifecycle: LifecycleRegistry) -> Unit) {
+        val registry by composeSaveableStateRegistryFlow.collectAsState()
+        if (registry != null) {
+            CompositionLocalProvider(LocalSaveableStateRegistry provides registry) {
+                content(composeComponent!!, TODO())
+            }
+        }
     }
 }
